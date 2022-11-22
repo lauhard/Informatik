4f40e90b8a76ffd81eec225816924832843cd3a2
lauhard.andreas <lauhard.andreas@gmail.com>
2022-11-22 21:24:33 +0100

    blub...

--- wechselautomat_hausübung/Program.cs
+++ wechselautomat_hausübung/Program.cs
@@ -29,6 +29,7 @@ do
         anzahlCoins = cents / coin_in_cents;
         rest = cents % coin_in_cents;
         int coin =  coin_in_cents / 100;
+        changeColor(ConsoleColor.DarkGray);
         Console.WriteLine($" {anzahlCoins} X {coin} Euro,  rest {rest} Cent ");
         if (rest > 0) cents = rest;
     }
@@ -39,6 +40,7 @@ do
         anzahlCoins = cents / coin_in_cents;
         rest = cents % coin_in_cents;
         int coin =  coin_in_cents / 100;
+        changeColor(ConsoleColor.DarkGray);
         Console.WriteLine($" {anzahlCoins} X {coin} Euro,  rest {rest} Cent ");
     }
     coin_in_cents = 50;
@@ -47,6 +49,7 @@ do
         anzahlCoins = rest / coin_in_cents;
         rest = rest % coin_in_cents;
         int coin = coin_in_cents;
+        changeColor(ConsoleColor.DarkYellow);
         Console.WriteLine($" {anzahlCoins} X {coin} Cent,  rest {rest} Cent ");
     }
 
@@ -56,6 +59,7 @@ do
         anzahlCoins = rest / coin_in_cents;
         rest = rest % coin_in_cents;
         int coin = coin_in_cents;
+        changeColor(ConsoleColor.DarkYellow);
         Console.WriteLine($" {anzahlCoins} X {coin} Cent,  rest {rest} Cent ");
     }
 
@@ -65,6 +69,7 @@ do
         anzahlCoins = rest / coin_in_cents;
         rest = rest % coin_in_cents;
         int coin = coin_in_cents;
+        changeColor(ConsoleColor.DarkYellow);
         Console.WriteLine($" {anzahlCoins} X {coin} Cent,  rest {rest} Cent ");
     }
 
@@ -74,6 +79,7 @@ do
         anzahlCoins = rest / coin_in_cents;
         rest = rest % coin_in_cents;
         int coin = coin_in_cents;
+        changeColor(ConsoleColor.DarkRed);
         Console.WriteLine($" {anzahlCoins} X {coin} Cent,  rest {rest} Cent ");
     }
 
@@ -83,6 +89,7 @@ do
         anzahlCoins = rest / coin_in_cents;
         rest = rest % coin_in_cents;
         int coin = coin_in_cents;
+        changeColor(ConsoleColor.DarkRed);
         Console.WriteLine($" {anzahlCoins} X {coin} Cent,  rest {rest} Cent ");
     }
 
@@ -92,6 +99,7 @@ do
         anzahlCoins = rest / coin_in_cents;
         rest = rest % coin_in_cents;
         int coin = coin_in_cents;
+        changeColor(ConsoleColor.DarkRed);
         Console.WriteLine($" {anzahlCoins} X {coin} Cent,  rest {rest} Cent ");
     }
 
@@ -110,186 +118,6 @@ do
 
 
 
-#region Helper
-static void renderZweiEuro(int anzahl)
-{
-    if (anzahl > 0)
-    {
-        string zweiE =
-      $"\n            2 EURO  x  {anzahl}" + "              \n" +
-      "\n               _ _ _" + "              " +
-      "\n           -          -" + "           " +
-      "\n         -             -" + "          " +
-      "\n        -     * * *     -" + "         " +
-      "\n       -     *     *     -" + "        " +
-      "\n       -         *       -" + "       " +
-      "\n       -       *         -" + "        " +
-      "\n        -    * * * *    -" + "         " +
-      "\n         -             -" + "          " +
-      "\n           -  _ _ _  -" + "          ";
-
-        changeColor(ConsoleColor.Gray);
-        Console.Write(zweiE + "\n\n");
-        changeColor(ConsoleColor.White);
-    }
-}
-static void renderEinEuro(int anzahl)
-{
-    if (anzahl > 0)
-    {
-        string einE =
-    $"\n            1 EURO  x  {anzahl}" + "              \n" +
-    "\n            _ - - - _" + "              " +
-    "\n         -             -" + "           " +
-    "\n        -       *       -" + "          " +
-    "\n       -        *        -" + "         " +
-    "\n       -        *        -" + "         " +
-    "\n       -        *        -" + "         " +
-    "\n        -       *       -" + "          " +
-    "\n         -             -" + "           " +
-    "\n           -  _ _ _  -" + "             ";
-
-        changeColor(ConsoleColor.Gray);
-        Console.Write(einE + "\n\n");
-        changeColor(ConsoleColor.White);
-    }
-}
-static void renderFuenfzigCent(int anzahl)
-{
-    if (anzahl > 0)
-    {
-        string fuenfzigC =
-    $"\n            50 CENT  x  {anzahl}" + "              \n" +
-    "\n           _ - - - - _" + "              " +
-    "\n         -             -" + "            " +
-    "\n        -  * * *  * * * -" + "           " +
-    "\n       -   *      *   *  -" + "          " +
-    "\n       -   * * *  *   *  -" + "          " +
-    "\n       -       *  *   *  -" + "          " +
-    "\n        -  * * *  * * * -" + "           " +
-    "\n         - _         _ -" + "            " +
-    "\n             - - - -" + "                ";
-
-        changeColor(ConsoleColor.DarkYellow);
-        Console.Write(fuenfzigC + "\n\n");
-        changeColor(ConsoleColor.White);
-    }
-}
-static void renderZwanzigCent(int anzahl)
-{
-    if (anzahl > 0)
-    {
-        string zwanzigC =
-        $"\n            20 CENT  x  {anzahl}" + "\n" +
-        "\n           _  - - -  _" + "            " +
-        "\n         -             -" + "          " +
-        "\n        -   * *   * * * -" + "         " +
-        "\n       -   *   *  *   *  -" + "        " +
-        "\n       -     *    *   *  -" + "        " +
-        "\n       -    *     *   *  -" + "        " +
-        "\n        -  * * *  * * * -" + "         " +
-        "\n         -             -" + "          " +
-        "\n           -  _ _ _  -" + "            ";
-
-        changeColor(ConsoleColor.DarkYellow);
-        Console.Write(zwanzigC + "\n\n");
-        changeColor(ConsoleColor.White);
-    }
-}
-static void renderZehnCent(int anzahl)
-{
-    if (anzahl > 0)
-    {
-        string zehnC =
-        $"\n           10 CENT  x  {anzahl}" + "\n" +
-        "\n            _ - - _" + "             " +
-        "\n         -           -" + "          " +
-        "\n        -   *  * * *  -" + "         " +
-        "\n       -    *  *   *   -" + "        " +
-        "\n       -    *  *   *   -" + "        " +
-        "\n       -    *  *   *   -" + "        " +
-        "\n        -   *  * * *  -" + "         " +
-        "\n         -           -" + "          " +
-        "\n            - _ _ -" + "             ";
-
-        changeColor(ConsoleColor.DarkYellow);
-        Console.Write(zehnC + "\n\n");
-        changeColor(ConsoleColor.White);
-    }
-}
-static void renderFuenfCent(int anzahl)
-{
-    if (anzahl > 0)
-    {
-        string fuenfC =
-        $"\n            5 CENT  x  {anzahl}" + "\n" +
-        "\n            _ - - _" + "             " +
-        "\n         -           -" + "          " +
-        "\n        -    * * *    -" + "         " +
-        "\n       -     *         -" + "        " +
-        "\n       -     * * *     -" + "        " +
-        "\n       -         *     -" + "        " +
-        "\n        -    * * *    -" + "         " +
-        "\n         -           -" + "          " +
-        "\n            - _ _ -" + "             ";
-
-        changeColor(ConsoleColor.DarkRed);
-        Console.Write(fuenfC + "\n\n");
-        changeColor(ConsoleColor.White);
-    }
-
-}
-static void renderZweiCent(int anzahl)
-{
-    if (anzahl > 0)
-    {
-        string zweiC =
-       $"\n             2 CENT  x  {anzahl}" + "\n" +
-        "\n             _ - - _" + "             " +
-        "\n          -           -" + "          " +
-        "\n         -     * *     -" + "         " +
-        "\n        -     *   *     -" + "        " +
-        "\n        -       *       -" + "        " +
-        "\n        -      *        -" + "        " +
-        "\n         -    * * *    -" + "         " +
-        "\n          -           -" + "          " +
-        "\n             - _ _ -" + "             ";
-
-        changeColor(ConsoleColor.DarkRed);
-        Console.Write(zweiC + "\n\n");
-        changeColor(ConsoleColor.White);
-    }
-}
-static void renderEinCent(int anzahl)
-{
-    if (anzahl > 0)
-    {
-        string einC =
-      $"\n              1 CENT  x  {anzahl}" + "\n" +
-        "\n               _" + "               " +
-        "\n           -       -" + "           " +
-        "\n         -     *     -" + "         " +
-        "\n        -      *      -" + "        " +
-        "\n        -      *      -" + "        " +
-        "\n        -      *      -" + "        " +
-        "\n         -     *     -" + "         " +
-        "\n           -   _   -" + "           ";
-        changeColor(ConsoleColor.DarkRed);
-        Console.Write(einC + "\n\n");
-        changeColor(ConsoleColor.White);
-    }
-}
 static void changeColor(ConsoleColor c) => Console.ForegroundColor = c;
-static bool mod0(int num, int b) => num % b == 0;
-static string? read() => Console.ReadLine();
-static bool laufWiederholen()
-{
-    changeColor(ConsoleColor.White);
-    Console.WriteLine("Wollen Sie das Programm wiederholen, J/N");
-    var wunsch = read().ToLower();
-    if (wunsch != "j" && wunsch != "n") return laufWiederholen();
-    else return wunsch == "j" ? true : false;
-}
-#endregion
 
 
